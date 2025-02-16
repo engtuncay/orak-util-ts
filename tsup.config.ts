36f156d52584a6f8471e0d1d5aad6d9b51f7ecf3
TuncayHp <tunc270@gmail.com>
2025-02-16 13:18:58 +0300

    up

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "tsup";
 import { minify } from "terser";
-import fs from "fs/promises";
+//import fs from "fs/promises";
+import { promises as fs } from "fs";
 //alternatif tanımlama
 //import { promises as fs } from "fs";
 
