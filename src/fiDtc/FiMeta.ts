d4f19b0fdcd0ee6bec8d6012f060061aa8321851
Tuncay <tunc270@gmail.com>
2025-07-05 10:41:13 +0300

    up

--- src/fiDtc/FiMeta.ts
+++ src/fiDtc/FiMeta.ts
@@ -1,11 +1,19 @@
 import { FiNumber } from "../fiCore/FiNumber";
 import { FiString } from "../fiCore/FiString";
-import {IFiMeta} from "./IFiMeta";
+import type { IFiMeta } from "./IFiMeta";
 
 export class FiMeta implements IFiMeta {
+
   fimTxKey?: string;
   fimTxValue?: string;
   fimLnKey?: number;
+
+  // Static Methods
+  static create(txKey: string): FiMeta {
+    let fiMeta = new FiMeta();
+    fiMeta.fimTxKey = txKey;
+    return fiMeta;
+  }
 
   // Getters
 
